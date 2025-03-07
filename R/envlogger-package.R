e534232751ad47f0f9d152d975b0c2978732a492
ruiseabra <ruisea@gmail.com>
2025-03-07 10:42:22 +0000

    Initial commit

---
+++ R/envlogger-package.R
@@ -0,0 +1,9 @@
+#' @keywords internal
+"_PACKAGE"
+
+## usethis namespace: start
+#' @importFrom magrittr %>%
+## usethis namespace: end
+NULL
+
+isdir <- path <- type <- id <- press <- pressure <- serial <- serials <- data <- overlap <- t0 <- t1 <- sh <- lvl <- mic <- temp <- quantile <- approx <- val <- avg <- .data <- facet_c <- facet_r <- time <- v_app <- accuracy <- code <- start_time <- time_diff_s <- long <- device <- name <- action <- status <- v <- samp_int_s <- samp_res_c <- dev <- NULL
